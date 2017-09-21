4c9885ac6a8587815bad563d2c598df1f4081301
David Glassanos <david@naturebox.com>
2017-09-20 23:53:45 -0700

    Adding landing page and all assets

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/landing', function () {
+    return view('landing');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
